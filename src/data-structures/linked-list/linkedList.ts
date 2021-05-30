dae0408d6f262ff20d7622617ac4b4fdad6c4edd
liushuai <liushuai@umu.com>
2021-05-31 00:59:02 +0800

    dev: ++

--- src/data-structures/linked-list/linkedList.ts
+++ src/data-structures/linked-list/linkedList.ts
@@ -1,70 +1,102 @@
 /**
  * @author liushuai
- * @description 实现链表
  */
 import { isEqual } from "../../util";
-class Node<NodeData = any> {
+type TMapFunction<NodeData = any> = (data: NodeData, list: LinkedList) => void;
+class LinkNode<NodeData = any> {
   constructor(
     public data: NodeData,
-    public next: Node<NodeData> | null = null
+    public next: LinkNode<NodeData> | null = null
   ) {}
 }
 class LinkedList<NodeData = any> {
-  public head: Node<NodeData> | null;
-  private size: number;
+  public head: LinkNode<NodeData> | null;
+  public size: number;
   constructor() {
     this.head = null;
-    this.size = 0;
   }
+
   public append(data: NodeData): LinkedList<NodeData> {
     if (this.head === null) {
-      this.head = new Node(data);
-      this.size += 1;
+      this.head = new LinkNode(data);
       return this;
     }
     let currentNode = this.head;
     while (currentNode.next !== null) {
       currentNode = currentNode.next;
     }
-    currentNode.next = new Node(data);
-    this.size += 1;
+    currentNode.next = new LinkNode(data);
     return this;
   }
   public prepend(data: NodeData): LinkedList<NodeData> {
-    const newNode = new Node(data);
+    const newNode = new LinkNode(data);
     newNode.next = this.head;
     this.head = newNode;
-    this.size += 1;
     return this;
   }
-  public removeNdoe(node: NodeData): LinkedList<NodeData> {
+  public clear(): LinkedList<NodeData> {
+    this.head = null;
+    return this;
+  }
+  public removeNode(data: NodeData): LinkedList<NodeData> {
     if (this.head === null) {
-      return null;
-    }
-    if (this.head.data === node) {
-      this.head = this.head.next;
-      node = null;
       return this;
     }
     let currentNode = this.head;
-    while (currentNode.next !== null) {
-      if (isEqual(currentNode.next.data, node)) {
+    while (currentNode.next.next !== null) {
+      if (isEqual(currentNode.next.data, data)) {
         currentNode.next = currentNode.next.next;
-        node = null;
-        this.size -= 1;
-        return this;
       }
       currentNode = currentNode.next;
     }
     return this;
   }
+  public reverse(): LinkedList<NodeData> {
+    let currentNode = this.head;
+    let prev = null;
+    while (currentNode !== null) {
+      const nextNode = currentNode.next;
+      currentNode.next = prev;
+      prev = currentNode;
+      currentNode = nextNode;
+    }
+    this.head = prev;
+    return this;
+  }
+
+  batchAppend(nodes: NodeData[]): LinkedList<NodeData> {
+    for (const item of nodes) {
+      this.append(item);
+    }
+    return this;
+  }
+
+  public forEach(f: TMapFunction<LinkNode<NodeData>>): LinkedList<NodeData> {
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      f(currentNode, this);
+      currentNode = currentNode.next;
+    }
+    return this;
+  }
 }
-const linkedList = new LinkedList<{ name: number }>();
+const linkedList = new LinkedList<{
+  name: string;
+}>();
 linkedList
-  .append({ name: 11 })
-  .append({ name: 22 })
-  .append({ name: 33 })
-  .prepend({ name: 0 })
-  .removeNdoe({ name: 11 });
+  .append({ name: "111" })
+  .append({ name: "222" })
+  .append({ name: "333" })
+  .prepend({ name: "000" });
+linkedList.batchAppend([
+  { name: "444" },
+  { name: "555" },
+  { name: "666" },
+  { name: "777" },
+]);
 console.log(JSON.stringify(linkedList));
+const reverseList = linkedList.reverse().prepend({ name: "-111" });
+reverseList.forEach((node) => {
+  console.log(node.data, "---data");
+});
 export default LinkedList;
